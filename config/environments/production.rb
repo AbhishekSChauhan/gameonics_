a5d9e1ee32e2833c1c63d2f0e19d1d32b8357208
Abhishek Singh <abhishek.uietpuchd@gmail.com>
2021-09-17 17:51:22 +0530

    Add heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.delivery_method = :smtp
-  host = 'https://stormy-castle-52072.herokuapp.com/' #replace with your own url
+  host = 'https://fathomless-earth-98069.herokuapp.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
